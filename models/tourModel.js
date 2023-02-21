b54162b8ddcc698dbfc58ada24f487acb584dc17
gabrielclteixeira <gabrielcalaca@hotmail.com>
2023-02-21 21:18:06 +0000

    Imported data to mongoose, much more to do

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,16 +5,58 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Tours must have names!'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'Tours must have durations!'],
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'Tours must have a max group size!'],
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'Tours must have difficulty!'],
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'Tours must have prices!']
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+       type: String,
+       trim: true
+    },
+    description: {
+        type: String,
+        required: [true, 'A tour must have a description!'],
+        trim: true
+     },
+     imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image!'],
+     },
+     images: {
+        type: [String],
+     },
+     createdAt: {
+        type: Date,
+        default: Date.now(),
+     },
+     startDates: {
+        type: [Date],
+     }
+    
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
