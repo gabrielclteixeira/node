ac53c135ce1e9c1bcb085288054176a708177d4b
gabrielclteixeira <gabrielcalaca@hotmail.com>
2023-02-23 20:15:56 +0000

    Added some more advanced filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const { all } = require("../app");
 const Tour = require("../models/tourModel");
 
@@ -5,13 +6,57 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
     try{
+        // eslint-disable-next-line node/no-unsupported-features/es-syntax
+        const queryObj = {...req.query};
+        const excludedField = ['page', 'sort', 'limit', 'fields'];
 
-        const allTours = await Tour.find();
+        excludedField.forEach(e => delete queryObj[e]) 
+
+        let queryStr = JSON.stringify(queryObj);
+
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, match => `$${match}`);
+
+        console.log(JSON.parse(queryStr));
+
+        let allTours = Tour.find(JSON.parse(queryStr));
+
+        if (req.query.sort){
+
+            const sortBy = req.query.sort.split(',').join(' ');
+
+            allTours = allTours.sort(sortBy);
+        } 
+        else{
+            allTours = allTours.sort('-createdAt');
+        }
+
+        if (req.query.fields){
+            const fields = req.query.sort.split(',').join(' ');
+
+            allTours = allTours.select(fields);
+        }
+        else{
+            allTours = allTours.select('-__v');
+        }
+
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        allTours = allTours.skip(skip).limit(limit);
+
+        if (req.query.page){
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) throw new Error('This page exceeds limit!');
+        }
+
+        const tours = await allTours;
 
         res.status(200).json({
             status: 'success',
+            results: tours.length,
             data: {
-                allTours
+                tours
             }
         })
 
@@ -19,7 +64,7 @@ exports.getAllTours = async (req, res) => {
     catch(err) {
         res.status(400).json({
             status: 'fail',
-            message: 'Something went wrong!'
+            message: err
         })
     }
 }
